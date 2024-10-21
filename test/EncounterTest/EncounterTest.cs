43f809191d28db5c73667b060bd868d2ac9606d5
Ines Barral <barral.mariaines@gmail.com>
2024-10-21 12:58:59 -0300

    Added encounter test file

---
+++ test/EncounterTest/EncounterTest.cs
@@ -0,0 +1,16 @@
+using Library;
+using NUnit.Framework;
+
+namespace EncounterTest;
+
+[TestFixture]
+[TestOf(typeof(Encounter))]
+public class EncounterTest
+{
+
+    [Test]
+    public void TestEncounter()
+    {
+        
+    }
+}
