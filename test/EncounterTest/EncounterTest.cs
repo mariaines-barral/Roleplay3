bfed39bd9636fcf751783d0f64694335686ae1ea
Ines Barral <barral.mariaines@gmail.com>
2024-10-21 13:57:27 -0300

    Added 2V2 test

--- test/EncounterTest/EncounterTest.cs
+++ test/EncounterTest/EncounterTest.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Library;
 using NUnit.Framework;
 using Ucu.Poo.RoleplayGame;
@@ -12,6 +13,8 @@ public class EncounterTest
     private MalignantDwarf enanoMalo;
     private Archer arqueroBueno;
     private RuffianWizard magoMalo;
+    private Knight caballeroBueno;
+    private Dwarf enanoBueno;
     private Encounter batalla;
     
     [SetUp]
@@ -21,6 +24,9 @@ public class EncounterTest
         enanoMalo = new MalignantDwarf("Enano Malo", 6);
         arqueroBueno = new Archer("Arquero Malo");
         magoMalo = new RuffianWizard("Mago malo", 5);
+        caballeroBueno = new Knight("Raul");
+        enanoBueno = new Dwarf("Dobby");
+        
         batalla = new Encounter();
         arqueroBueno.AddItem(new Sword());
     }
@@ -29,5 +35,22 @@ public class EncounterTest
     public void AddToEnocunter()
     {
         
+    }
+
+    [Test]
+    public void Test2V2()
+    {
+        batalla.AddToEncounter(magoMalo);
+        batalla.AddToEncounter(enanoMalo);
+        batalla.AddToEncounter(caballeroBueno);
+        batalla.AddToEncounter(enanoBueno);
+        batalla.DoEncounter();
+        Assert.That(enanoBueno.VP, Is.EqualTo(0));
+        Assert.That(caballeroBueno.VP, Is.EqualTo(0));
+        Assert.That(enanoBueno.Health, Is.EqualTo(0));
+        Assert.That(caballeroBueno.Health, Is.EqualTo(0));
+        Assert.That(batalla.Heroes.Count, Is.EqualTo(0));
+        Assert.That(magoMalo.Health, Is.EqualTo(100));
+        Assert.That(enanoMalo.Health, Is.EqualTo(100));
     }
 }
