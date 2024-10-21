ba6cd7dcea7c2eb9b947a25c0313fa2b0f0d819a
matiasdcl <matiasdacostaleites@gmail.com>
2024-10-21 14:06:10 -0300

    tests personajes

--- test/EncounterTest/EncounterTest.cs
+++ test/EncounterTest/EncounterTest.cs
@@ -12,6 +12,7 @@ public class EncounterTest
     private MalignantDwarf enanoMalo;
     private Archer arqueroBueno;
     private RuffianWizard magoMalo;
+    private DespicableKnight caballeroMalandro;
     private Encounter batalla;
     
     [SetUp]
@@ -21,6 +22,7 @@ public class EncounterTest
         enanoMalo = new MalignantDwarf("Enano Malo", 6);
         arqueroBueno = new Archer("Arquero Malo");
         magoMalo = new RuffianWizard("Mago malo", 5);
+        caballeroMalandro = new DespicableKnight("caballero Malandro", 5);
         batalla = new Encounter();
         arqueroBueno.AddItem(new Sword());
     }
@@ -29,5 +31,28 @@ public class EncounterTest
     public void AddToEnocunter()
     {
         
+    }
+
+    [Test]
+    public void Test1V1Enocunter()
+    {
+        batalla.AddToEncounter(magoBueno);
+        batalla.AddToEncounter(enanoMalo);
+        batalla.DoEncounter();
+        Assert.That(batalla.Enemies.Count, Is.EqualTo(0));
+        Assert.That(magoBueno.Health, Is.EqualTo(100));
+
+    }
+
+    [Test]
+    public void Test2v1Encounter()
+    {
+        batalla.AddToEncounter(magoBueno);
+        batalla.AddToEncounter(enanoMalo);
+        batalla.AddToEncounter(caballeroMalandro);
+        batalla.DoEncounter();
+        Assert.That(batalla.Enemies.Count, Is.EqualTo(0));
+        Assert.That(magoBueno.Health, Is.EqualTo(100));
+
     }
 }
